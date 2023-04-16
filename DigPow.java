968f2cf32ef225c92149f007364a812f04b54028
ampats04 <jeremyandyampatin@gmail.com>
2023-04-16 22:02:58 +0800

    DigPow Java

---
+++ DigPow.java
@@ -0,0 +1,12 @@
+public class DigPow {
+
+    public static long digPow(int n, int p) {
+
+        return -1;
+    }
+
+    public static void main(String... args) {
+
+        System.out.print(digPow(3, 4));
+    }
+}
