1d07b9a58594a5de63c9ed51fdbf95d3b3690678
ampats04 <jeremyandyampatin@gmail.com>
2023-06-16 19:37:03 +0800

    Mapping

--- DeadFish.java
+++ DeadFish.java
@@ -1,24 +1,37 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class DeadFish {
 
     public static int[] parse(String data) {
 
         int len = data.length();
-        int[] d = new int[len];
+
+        Map<Character, Integer> map = new HashMap<>();
 
         for (int i = 0; i < len; i++) {
 
-            d[i] = data.charAt(i);
+            char c = data.charAt(i);
+
+            map.put(c, i);
 
         }
 
-        return d;
+        for (Entry<Character, Integer> entry : map.entrySet()) {
+            char key = entry.getKey();
+            int value = entry.getValue();
+
+            System.out.println(key + ": " + value);
+        }
+
+        return null;
     }
 
     public static void main(String... args) {
 
-        int[] result = parse("asdf");
+        int[] result = parse("iiisdoso");
 
         System.out.print(Arrays.toString(result));
     }
